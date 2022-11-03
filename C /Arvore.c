df8e5c46ca057cde9ba416ad3a0d5e8abaa5cd5b
Danilo Macedo Azevedo Fernandes <101714528+Macwdo@users.noreply.github.com>
2022-11-03 08:29:32 -0300

    Update Arvore.c

--- C /Arvore.c
+++ C /Arvore.c
@@ -19,3 +19,25 @@ struct Node *novoNo(char k){
     node->direita = node->esquerda= NULL;
     return node;
 }
+/* Esta função testa se a árvore binária é uma árvore binária completo.*/
+
+bool arvoreCheia(stuct Node* root){
+    // Se a arvore estiver vazia
+    if (root == NULL)
+    return true;
+    
+    // Se o nó for uma folha
+    if (root->esquerda == NULL && root->direita == NULL)
+    return true;
+    
+    /* Se ambos não forem NULL e as subarvores estão cheias. */
+    if((root->esquerda)&&(root->direita))
+    return (arvoreCheia(root->esquerda)&& arvoreCheia(root->direita));
+    
+    // Nos chegamos quando nenhuma das condições funcionam
+    return false;
+    
+}
+
+
+
